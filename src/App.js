025a0fe28b2bc23384d5cb86fc93014a2847faed
Farhan-2005-etw <farhan2005etw@gmail.com>
2025-02-23 13:17:59 +0530

    Adding Use Reducer Hook

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import Heading from "./components/Heading.js";
 import Todoo from "./components/Todoo.js";
 import TodooItems from "./components/TodooItems.js";
 import HomeMessage from "./components/HomeMessage.js";
 import { TodoContext } from "./store/Todo-Item_context.js";
 
+const todoItemsReducer = (currTodoItems, action) => {
+  let newItemAdd = currTodoItems;
+  if (action.type === 'NEW_ITEM') {
+   newItemAdd = [
+      ...currTodoItems,
+      { Name: action.payload.itemName, dueDate: action.payload.itemDueDate },
+    ];
+  } else if (action.type === 'DELETE_ITEM') {
+    newItemAdd = currTodoItems.filter(
+      (item) => item.Name !== action.payload.itemName
+    );
+    //setTodooList(toDeleteItem);
+
+  }
+  return newItemAdd;
+} 
+
 function App() {
   let initialtodooList = [];
 
-  const [todooList, setTodooList] = useState(initialtodooList);
+  //const [todooList, setTodooList] = useState(initialtodooList);
+  const [todooList, dispatchTodooItems] = useReducer(todoItemsReducer, [])
 
   const addNewItem = (itemName, itemDueDate) => {
-    setTodooList((currValue) => {
+    const newItemAction = {
+      type: 'NEW_ITEM',
+      payload : {
+        itemName,
+        itemDueDate
+      }
+    };
+    dispatchTodooItems(newItemAction);
+
+   
+   
+    /* setTodooList((currValue) => {
       const newItemAdd = [
         ...currValue,
         { Name: itemName, dueDate: itemDueDate },
       ];
       return newItemAdd;
-    });
+    });*/
   };
 
   const deleteItem = (todooItemName) => {
-    const toDeleteItem = todooList.filter(
-      (item) => item.Name !== todooItemName
-    );
-    setTodooList(toDeleteItem);
+    
+    
+    const deleteItemAction = {
+      type: 'DELETE_ITEM',
+      payload : {
+        itemName : todooItemName,
+      }
+    };
+    dispatchTodooItems(deleteItemAction);
+
+
+
+
+    
   };
 
 
